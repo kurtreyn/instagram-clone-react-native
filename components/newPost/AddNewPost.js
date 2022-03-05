b8f4d432cc71f4386456ccf7cc690bda86357bf4
kurtreyn <kurtreyn@yahoo.com>
2022-03-05 12:34:43 -0500

    continue working on AddNewPost

--- components/newPost/AddNewPost.js
+++ components/newPost/AddNewPost.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 const AddNewPost = () => {
   return (
-    <View>
-      <Text style={{ color: 'white' }}>ADD NEW POST</Text>
+    <View style={styles.container}>
+      <Header />
+      {/* <PostForm/> */}
     </View>
   );
 };
 
+const Header = () => (
+  <View style={styles.headerContainer}>
+    <TouchableOpacity>
+      <Image
+        source={{
+          uri: 'https://img.icons8.com/ios-glyphs/90/ffffff/back.png',
+        }}
+        style={{ width: 30, height: 30 }}
+      />
+    </TouchableOpacity>
+    <Text style={styles.headerText}>NEW POST</Text>
+  </View>
+);
+
 export default AddNewPost;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 10,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 20,
+    marginLeft: 27.5,
+  },
+});
