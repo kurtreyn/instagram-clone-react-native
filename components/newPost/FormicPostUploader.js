215d7c60652ff240c5a19983a8e09e3806637bc7
kurtreyn <kurtreyn@yahoo.com>
2022-03-06 18:19:37 -0500

    finish with initial FormicPostUploader component

--- components/newPost/FormicPostUploader.js
+++ components/newPost/FormicPostUploader.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 import * as Yup from 'yup';
-import { Formik } from 'formik';
+import { ErrorMessage, Formik } from 'formik';
+import { Divider } from 'react-native-elements';
+
+import imgPlaceholder from '../../assets/img-placeholder.jpeg';
+
+const PLACEHOLDER_IMG = Image.resolveAssetSource(imgPlaceholder).uri;
 
 const uploadPostSchema = Yup.object().shape({
   imageUrl: Yup.string().url().required('A URL is required'),
   caption: Yup.string().max(2200, 'Caption has reached the character limit'),
 });
-
-const PLACEHOLDER_IMG =
-  'https://www.brownweinraub.com/wp-content/uploads/2017/09/placeholder.jpg';
 
 const FormicPostUploader = () => {
   const [thumbnailUrl, setThumbnail] = useState(PLACEHOLDER_IMG);
@@ -19,24 +21,58 @@ const FormicPostUploader = () => {
       initialValues={{ caption: '', imageUrl: '' }}
       onSubmit={(values) => console.log(values)}
       validationSchema={uploadPostSchema}
+      validateOnMount={true}
     >
-      {({ handleBlur, handleChange, handleSubmit, values, erros, isValid }) => (
+      {({
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        values,
+        errors,
+        isValid,
+      }) => (
         <>
-          <View>
+          <View
+            style={{
+              margin: 20,
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+            }}
+          >
             <Image
-              source={{ uri: PLACEHOLDER_IMG }}
+              source={{ uri: thumbnailUrl ? thumbnailUrl : PLACEHOLDER_IMG }}
               style={{ width: 100, height: 100 }}
             />
 
-            <TextInput
-              placeholder="Write a caption..."
-              placeholderTextColor="gray"
-            />
-            <TextInput
-              placeholder="Enter Image URL"
-              placeholderTextColor="gray"
-            />
+            <View style={{ flex: 1, marginLeft: 12 }}>
+              <TextInput
+                style={{ color: 'white', fontSize: 20 }}
+                placeholder="Write a caption..."
+                placeholderTextColor="gray"
+                multiline={true}
+                onChangeText={handleChange('caption')}
+                onBlur={handleBlur('caption')}
+                value={values.caption}
+              />
+            </View>
           </View>
+          <Divider width={0.2} orientation="vertical" />
+          <TextInput
+            style={{ color: 'white', fontSize: 18 }}
+            placeholder="Enter Image URL"
+            placeholderTextColor="gray"
+            onChangeText={handleChange('imageUrl')}
+            onBlur={handleBlur('imageUrl')}
+            value={values.imageUrl}
+            onChange={(e) => setThumbnail(e.nativeEvent.text)}
+          />
+          {errors.imageUrl && (
+            <Text style={{ fontSize: 10, color: 'red' }}>
+              {errors.imageUrl}
+            </Text>
+          )}
+
+          <Button onPress={handleSubmit} title="Share" dasabled={!isValid} />
         </>
       )}
     </Formik>
